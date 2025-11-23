5ed651e421c3a44acbb1ac8ae88c773fe9ac9d3f
MOXHATKA <MOXHATKA@protonmail.com>
2025-11-23 17:47:04 +0300

    Обновление работы с состояниями

--- pkg/bot/state.go
+++ pkg/bot/state.go
@@ -5,6 +5,11 @@ import (
 	"gote/pkg/types"
 )
 
+type StateStore struct {
+	States     *States
+	UsersState *UsersState
+}
+
 type State struct {
 	Name   string
 	Handle Handler
@@ -14,13 +19,26 @@ type States map[string]*State
 type UsersState map[int64]*State
 type Handler func(context.Context, *types.Update, *Bot)
 
-func (bot *Bot) OnState(name string, handler Handler) {
-	(*bot.States)[name] = &State{
-		Name:   name,
-		Handle: handler,
+func NewStateStore() *StateStore {
+	return &StateStore{
+		States:     &States{},
+		UsersState: &UsersState{},
 	}
 }
 
-func (bot *Bot) SetState(id int64, name string) {
-	(*bot.UsersState)[id] = (*bot.States)[name]
+func (s *StateStore) NewState(name string, handler Handler) *State {
+	state := &State{
+		Name:   name,
+		Handle: handler,
+	}
+	(*s.States)[name] = state
+	return state
+}
+
+func (s *StateStore) SetState(id int64, name string) {
+	(*s.UsersState)[id] = (*s.States)[name]
+}
+
+func (s *StateStore) OnCommand(command string, state *State) {
+	(*s.States)[command] = state
 }
