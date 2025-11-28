5ec6e4b2d0e70b4d41cd45620cf862036dfa648e
MOXHATKA <MOXHATKA@protonmail.com>
2025-11-28 10:52:50 +0300

    Убран лишний fmt

--- pkg/types/cast.go
+++ pkg/types/cast.go
@@ -2,19 +2,16 @@ package types
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 func CastTo[T any](data any) (*T, error) {
 	jsonbody, err := json.Marshal(data)
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 
 	result := new(T)
 	if err := json.Unmarshal(jsonbody, &result); err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 
